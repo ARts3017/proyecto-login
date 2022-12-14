79761d9fe74e8dd20310848fb7362b9e47738b1f
ARts3017 <36316564+ARts3017@users.noreply.github.com>
2022-12-13 19:03:57 -0600

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,6 +1,5 @@
 create table login(
-
-id int not null auto_increment, 
+id int not null auto_increment primary key, 
 ususario varchar(50) not null,
-password varchar(50) not null
+pass varchar(50) not null
 );
